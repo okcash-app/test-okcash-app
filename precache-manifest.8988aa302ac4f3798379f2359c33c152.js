4c398a030e4f692bc41eec9513c4ef4024d1de9e
ck <969211472@qq.com>
2021-03-09 14:23:56 +0800

    0.0.9

---
+++ precache-manifest.8988aa302ac4f3798379f2359c33c152.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfff4c997743f5123f6691647fad101",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/index.html"
+  },
+  {
+    "revision": "a9db320a61fa345e18a2",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/css/main.44bf6f08.chunk.css"
+  },
+  {
+    "revision": "baedbbe79ba3a3a81883",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/0.9cac23db.chunk.js"
+  },
+  {
+    "revision": "72523038cb87e3cc1152",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/10.174c6eea.chunk.js"
+  },
+  {
+    "revision": "9b66e772f6a71a25c7a7",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/3.3003d760.chunk.js"
+  },
+  {
+    "revision": "35d5344ccada58f363ecb1fca8ade690",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/3.3003d760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf41930d2c23534d91e2",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/4.4e36dbcf.chunk.js"
+  },
+  {
+    "revision": "56652795f8d89e599df5",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/5.f2b0f1d3.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/5.f2b0f1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54dd63b3f6e84408e8bc",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/6.ae3891d9.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/6.ae3891d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d07879b8791a26220db",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/7.61bd8b26.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/7.61bd8b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b23613a568b415a4ad",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/8.fc0dd47b.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/8.fc0dd47b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d1c6b68aaa0a06681e",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/9.2b5caa0a.chunk.js"
+  },
+  {
+    "revision": "a9db320a61fa345e18a2",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/main.96227567.chunk.js"
+  },
+  {
+    "revision": "167f438363a9264010b7",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/js/runtime-main.9c69181c.js"
+  },
+  {
+    "revision": "daab00421bcef2a01c7f5766d58b8af4",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/BTC.daab0042.png"
+  },
+  {
+    "revision": "17db7fc28ea8f9cc1b19f24c67b10c60",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/DAI.17db7fc2.png"
+  },
+  {
+    "revision": "06869537b8d89d023aacc414fa8a5b02",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/DOT.06869537.png"
+  },
+  {
+    "revision": "1da7c73b1318f2bc79806ee4e2b8a480",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/ETH.1da7c73b.png"
+  },
+  {
+    "revision": "e958d605da4fa7493936cfbdb5e23d03",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/OKB.e958d605.png"
+  },
+  {
+    "revision": "e842d4437c1314b87b1e55002fe3b6a9",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/OKT.e842d443.png"
+  },
+  {
+    "revision": "c76b33ca42c5730ab77f3341ce9764a7",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/USDC.c76b33ca.png"
+  },
+  {
+    "revision": "378af2a7f579c2ae36fac5afd84889a2",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/USDT.378af2a7.png"
+  },
+  {
+    "revision": "721a68dfbb33916d09a883ced8b99354",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/basis-bond-logo.721a68df.png"
+  },
+  {
+    "revision": "954db61c6f984ff99360b65efeecfe06",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/basis-cash-logo.954db61c.png"
+  },
+  {
+    "revision": "ee73b5ad2cc77c3ad301bbbfb2b38b5b",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/basis-share-logo.ee73b5ad.png"
+  },
+  {
+    "revision": "6e4da90ec3c1a8d357fe7f09640bfb49",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/hammer.6e4da90e.png"
+  },
+  {
+    "revision": "f536a79e717e864a71b6e0765917ba5a",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/logo.f536a79e.png"
+  },
+  {
+    "revision": "f8fec857dc3b8e244e4c910c550b1df8",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/sUSD.f8fec857.png"
+  },
+  {
+    "revision": "88ae1ab3f40535ebbd248d03a7aab473",
+    "url": "https://cdn.jsdelivr.net/gh/okcash-app/test-okcash-app@0.0.9/static/media/ycrv.88ae1ab3.png"
+  }
+]);
