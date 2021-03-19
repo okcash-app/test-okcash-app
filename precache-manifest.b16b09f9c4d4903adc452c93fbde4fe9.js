d0070cf22b0b79a55d2da8cad75778c94e796b37
ck <969211472@qq.com>
2021-03-19 10:35:33 +0800

    0.0.27

---
+++ precache-manifest.b16b09f9c4d4903adc452c93fbde4fe9.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805a487a8ab8d0a5ec340aa95b1afe6b",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/index.html"
+  },
+  {
+    "revision": "2bdfb5eadec5e4fdfa61",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/css/main.44bf6f08.chunk.css"
+  },
+  {
+    "revision": "0217298fc1ee01a167de",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/0.fbb24df8.chunk.js"
+  },
+  {
+    "revision": "2fbedcfc55a4e2797713",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/10.58418027.chunk.js"
+  },
+  {
+    "revision": "66cfb45dede574f1559b",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/3.8a3a006f.chunk.js"
+  },
+  {
+    "revision": "a69207ddb1e9cf283c172cc8daf37769",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/3.8a3a006f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fefa6555293ce3ffa941",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/4.f4e3ba2b.chunk.js"
+  },
+  {
+    "revision": "a4f391bca6d6feeb08a6",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/5.cffb289c.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/5.cffb289c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8859ba31d9c6249ed02f",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/6.6747f3a8.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/6.6747f3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad4317c4e25bdfc42393",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/7.f3c125c8.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/7.f3c125c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1466e55eff6e692219c",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/8.b46fdc2a.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/8.b46fdc2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ca6e0ec5aa2350560c",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/9.481af61a.chunk.js"
+  },
+  {
+    "revision": "2bdfb5eadec5e4fdfa61",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/main.533c45b4.chunk.js"
+  },
+  {
+    "revision": "e024bd8d001e8d09babb",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/js/runtime-main.a9adda88.js"
+  },
+  {
+    "revision": "daab00421bcef2a01c7f5766d58b8af4",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/BTC.daab0042.png"
+  },
+  {
+    "revision": "17db7fc28ea8f9cc1b19f24c67b10c60",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/DAI.17db7fc2.png"
+  },
+  {
+    "revision": "06869537b8d89d023aacc414fa8a5b02",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/DOT.06869537.png"
+  },
+  {
+    "revision": "1da7c73b1318f2bc79806ee4e2b8a480",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/ETH.1da7c73b.png"
+  },
+  {
+    "revision": "e958d605da4fa7493936cfbdb5e23d03",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/OKB.e958d605.png"
+  },
+  {
+    "revision": "c76b33ca42c5730ab77f3341ce9764a7",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/USDC.c76b33ca.png"
+  },
+  {
+    "revision": "378af2a7f579c2ae36fac5afd84889a2",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/USDT.378af2a7.png"
+  },
+  {
+    "revision": "39fdecf03acd902b2ee01537130b547d",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/WOKT.39fdecf0.png"
+  },
+  {
+    "revision": "721a68dfbb33916d09a883ced8b99354",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/basis-bond-logo.721a68df.png"
+  },
+  {
+    "revision": "f536a79e717e864a71b6e0765917ba5a",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/basis-cash-logo-1.f536a79e.png"
+  },
+  {
+    "revision": "954db61c6f984ff99360b65efeecfe06",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/basis-cash-logo.954db61c.png"
+  },
+  {
+    "revision": "ee73b5ad2cc77c3ad301bbbfb2b38b5b",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/basis-share-logo.ee73b5ad.png"
+  },
+  {
+    "revision": "6e4da90ec3c1a8d357fe7f09640bfb49",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/hammer.6e4da90e.png"
+  },
+  {
+    "revision": "f536a79e717e864a71b6e0765917ba5a",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/logo.f536a79e.png"
+  },
+  {
+    "revision": "f8fec857dc3b8e244e4c910c550b1df8",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/sUSD.f8fec857.png"
+  },
+  {
+    "revision": "385ac0f6a8a21e0c6ddc0d3bef1f1fcb",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/stcLpLogo.385ac0f6.png"
+  },
+  {
+    "revision": "c3d28d2ac845585facc29a84767f6f25",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/stsLogo.c3d28d2a.png"
+  },
+  {
+    "revision": "5868db14c5ff9bb6f7fac8be76ad9f07",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/stsLpLogo.5868db14.png"
+  },
+  {
+    "revision": "88ae1ab3f40535ebbd248d03a7aab473",
+    "url": "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@0.0.27/static/media/ycrv.88ae1ab3.png"
+  }
+]);
