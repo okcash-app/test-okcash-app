5cff426d541a183e8e1ed6268e12427c6ef8ed78
kusofte-chap <969211472@qq.com>
2021-03-23 06:23:49 +0000

    Deploy v0.0.41

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@v0.0.40/precache-manifest.80897e042418d1def1cbc17899924ba2.js"
+  "https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@v0.0.41/precache-manifest.c0b8f8e0a964598fe77067b1673428c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@v0.0.40/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://cdn.jsdelivr.net/gh/stcash-app/stcash-app@v0.0.41/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
